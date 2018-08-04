5ff62cc46f953e5eb13299709750abb17f992c53
Marco Porsch <marco.porsch@posteo.de>
2018-08-04 08:33:14 +0200

    implement terminal registration/deregistration

--- message_bus.h
+++ message_bus.h
@@ -1,16 +1,86 @@
+#include <algorithm>
+#include <cassert>
 #include <future>
 #include <memory>
+#include <vector>
+
+template<typename Message>
+class MessageBusTerminal;
 
 namespace message_bus_detail
 {
   template<typename Message>
   struct MessageBusImpl
   {
+    using Terminal = MessageBusTerminal<Message>;
+    using MessagePtr = typename Terminal::MessagePtr;
+
+    struct TerminalImpl
+    {
+      Terminal *parent; // parent address used as ID
+
+      TerminalImpl(Terminal *parent)
+        : parent(parent)
+      {}
+    };
+
+    struct Terminals : public std::vector<TerminalImpl>
+    {
+      typename std::vector<TerminalImpl>::iterator find(Terminal *parent)
+      {
+        return std::find_if(this->begin(), this->end(),
+          [&](TerminalImpl const &terminal) -> bool
+          {
+            return (parent == terminal.parent);
+          });
+      }
+    };
+
+    Terminals terminals;
+    std::mutex mtx;
+
     MessageBusImpl()
     {}
 
     ~MessageBusImpl()
-    {}
+    {
+      // all terminals must be closed before destroying the bus
+      assert(terminals.empty());
+    }
+
+    template<typename... Args>
+    std::future<void> tx(Terminal *parent, Args... args)
+    {
+      return std::future<void>();
+    }
+
+    MessagePtr rx_nonblocking(Terminal *parent)
+    {
+      return std::make_shared<Message>();
+    }
+
+    std::future<MessagePtr> rx_blockable(Terminal *parent)
+    {
+      std::promise<MessagePtr> promise;
+      promise.set_value(std::make_shared<Message>());
+      return promise.get_future();
+    }
+
+    void CreateTerminal(Terminal *parent)
+    {
+      std::lock_guard<std::mutex> lock(mtx);
+
+      terminals.emplace_back(parent);
+    }
+
+    void CloseTerminal(Terminal *parent)
+    {
+      std::lock_guard<std::mutex> lock(mtx);
+
+      auto const it = terminals.find(parent);
+      assert(it != std::end(terminals));
+      (void)terminals.erase(it);
+    }
   };
 }
 
@@ -28,28 +98,30 @@ public:
 private:
   MessageBusTerminal(std::shared_ptr<message_bus_detail::MessageBusImpl<Message>> impl)
     : m_impl(std::move(impl))
-  {}
+  {
+    m_impl->CreateTerminal(this);
+  }
 
 public:
   ~MessageBusTerminal()
-  {}
+  {
+    m_impl->CloseTerminal(this);
+  }
 
   template<typename... Args>
   std::future<void> tx(Args... args)
   {
-    return std::future<void>();
+    return m_impl->tx(this, std::forward<Args>(args)...);
   }
 
   MessagePtr rx_nonblocking()
   {
-    return std::make_shared<Message>();
+    return m_impl->rx_nonblocking(this);
   }
 
   std::future<MessagePtr> rx_blockable()
   {
-    std::promise<MessagePtr> promise;
-    promise.set_value(std::make_shared<Message>());
-    return promise.get_future();
+    return m_impl->rx_blockable(this);
   }
 
 private:
